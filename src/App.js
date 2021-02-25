19548ebe006bdbe9c490ad2c0cbefd5dd22cdc0d
swapnil0709 <swapnilsinha20@gmail.com>
2021-02-25 20:01:54 +0530

    Add Nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
