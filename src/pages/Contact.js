81633649e38c39262e0949b578fb1ae280973f4a
swapnil0709 <swapnilsinha20@gmail.com>
2021-02-27 00:03:26 +0530

    Add contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,74 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAnim } from "../animation";
+import styled from "styled-components";
+
 const Contact = () => {
   return (
-    <motion.div
+    <StyledContact
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
+      style={{ background: "#fff" }}
     >
-      <h1>Contact</h1>
-    </motion.div>
+      <StyledTitle>
+        <StyledHide>
+          <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+        </StyledHide>
+      </StyledTitle>
+      <div>
+        <StyledHide>
+          <StyledSocial variants={titleAnim}>
+            <StyledCircle />
+            <h2>Send us a message</h2>
+          </StyledSocial>
+        </StyledHide>
+        <StyledHide>
+          <StyledSocial variants={titleAnim}>
+            <StyledCircle />
+            <h2>Email us</h2>
+          </StyledSocial>
+        </StyledHide>
+        <StyledHide>
+          <StyledSocial variants={titleAnim}>
+            <StyledCircle />
+            <h2>Social Media</h2>
+          </StyledSocial>
+        </StyledHide>
+      </div>
+    </StyledContact>
   );
 };
+
+const StyledContact = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const StyledTitle = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+
+const StyledHide = styled.div`
+  overflow: hidden;
+`;
+
+const StyledCircle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
+
+const StyledSocial = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default Contact;
