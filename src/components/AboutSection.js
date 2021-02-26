6d469d01b8729f51ff5d8a7aae1e2c62dd5d50d2
swapnil0709 <swapnilsinha20@gmail.com>
2021-02-26 16:55:12 +0530

    Add about section Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import home1 from "../img/home1.png";
-import styled from "styled-components";
+import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 import {
   StyledAbout,
   StyledDescription,
@@ -13,25 +14,25 @@ const AboutSection = () => {
       <StyledDescription>
         <div className="title">
           <StyledHide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </StyledHide>
           <StyledHide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </StyledHide>
           <StyledHide>
-            <h2>true.</h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </StyledHide>
         </div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills
-        </p>
-        <button>Contact us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact us</motion.button>
       </StyledDescription>
       <StyledImage>
-        <img src={home1} alt="Guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="Guy with a camera" />
       </StyledImage>
     </StyledAbout>
   );
